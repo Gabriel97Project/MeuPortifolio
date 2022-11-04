21d6b8b91ac264bbabc74605b5501e1e3dbc0c11
Gabriel97Project <gabriel97tomaz@hotmail.com>
2022-11-04 19:57:38 +0000

    atualizando portifolio

--- src/components/myFuture/myFuture.tsx
+++ src/components/myFuture/myFuture.tsx
@@ -1,12 +1,52 @@
 import React from 'react'
-import { MyFutureStyle } from './myFutureStyle'
+import { MyFutureSonStyle, MyFutureStyle, MyFutureTopThreeStyle, MyFutureTopOneStyle, MyFutureTopTwoStyle } from './myFutureStyle'
+import { FaDesktop } from "react-icons/fa";
+const MyFuture = () => {
+    return (
+        <MyFutureStyle>
+            <MyFutureSonStyle>
+                <MyFutureTopOneStyle>
+                    <div className='divOne'>
+                        <h2>Sempre evoluir:</h2>
+                        <p>Pretendo continuar meus estudos na area de desenvolvimento, buscando sempre me atualizar com
+                            as tecnologias mais recentes. Após ingressar no mercado de trabalho, pretendo fazer uma graduação
+                            em desenvolvimento de sistemas.
+                        </p>
+                    </div>
+                    <div className='divTwo'>
 
-const MyFuture =()=> {
-  return (
-    <MyFutureStyle>
+                    </div>
 
-    </MyFutureStyle>
-  )
+                </MyFutureTopOneStyle>
+                <MyFutureTopTwoStyle>
+
+                </MyFutureTopTwoStyle>
+                <MyFutureTopThreeStyle>
+                    <div className='divOne'>
+                        <h2>Metas para 2023:</h2>
+                        <p>
+                            -Conseguir meu primeiro emprego na area de TI<br />
+                            -Estudar Micro Front-end<br />
+                            -Estudar React native <br />
+                            -Desevolver um aplicativo <br />
+                            -Aprimorar meu inglês <br />
+                        </p>
+                    </div>
+                    <div className='divTwo'>
+                        <h2>Metas para o futuro</h2>
+                        <p>
+                            -Estudar back-end<br />
+                            -desenvolver um grande site utilizando meu conhecimento front/back-end <br />
+                            -Ter a oportunidade de ensinar novos Devs <br />
+                            -Trabalhar no exterior como desenvolvedor
+                        </p>
+                    </div>
+
+                </MyFutureTopThreeStyle>
+            </MyFutureSonStyle>
+
+        </MyFutureStyle>
+    )
 }
 
 export default MyFuture
--- src/components/myFuture/myFutureStyle.ts
+++ src/components/myFuture/myFutureStyle.ts
@@ -4,8 +4,100 @@ import styled from "styled-components"
 
 export const MyFutureStyle = styled.div`
 
-    widht:100%;
+    width:100%;
     height:600px;
-    background-color:red;
+    //background-color:blue;
+    display:flex;
+    flex-direction:row;
+    .PcIconClass{
+        width:10%;
+        height:90%;
+       div{
+            width:50%;
+            height:100%;
+            background-color:yellow;
+        }
+    }
+`
 
+export const MyFutureSonStyle = styled.div`
+
+    width:100%;
+    height:100%;
+    background-color:white;
+    display:flex;
+    flex-direction:row;
+    margin-left:5%;
+    
+`
+export const MyFutureTopOneStyle = styled.div`
+
+    width:35%;
+    height:100%;
+    background-color:blue;
+    display:flex;
+    flex-direction:column;
+    justify-content:center ;
+    text-align:justify ;
+    .divOne{
+        height:50%;
+        background-color:green;
+        display:flex;
+        flex-direction:column;
+        align-items:center ;
+            p{
+            font-size:20px;
+            width:90%;
+            }
+    }
+    .divTwo{
+        height:50%;
+        background-color:yellow;
+        display:flex;
+        flex-direction :column;
+        align-items:center ;
+            p{
+            font-size:20px;
+            margin-left:5%;
+            }
+        }
+    
+`
+export const MyFutureTopTwoStyle = styled.div`
+
+    width:25%;
+    height:100%;
+    background-color:black;
+    
+`
+export const MyFutureTopThreeStyle = styled.div`
+
+    width:35%;
+    height:100%;
+    background-color:white;
+    display:flex;
+    flex-direction:column;
+
+    .divOne{
+        height:50%;
+        background-color:red;
+        display:flex;
+        flex-direction:column;
+        align-items:center ;
+            p{
+            font-size:20px;
+            }
+    }
+    .divTwo{
+        height:50%;
+        background-color:beige;
+        display:flex;
+        flex-direction :column;
+        align-items:center ;
+            p{
+            font-size:20px;
+            margin-left:5%;
+            }
+    }
+    
 `
